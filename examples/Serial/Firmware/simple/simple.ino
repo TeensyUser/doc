5a703db606ef24a7e415102495d960224c9cae21
luni64 <lutz.niggl@lunoptics.com>
2020-03-16 21:04:27 +0100

    Added serial example

---
+++ examples/Serial/Firmware/simple/simple.ino
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+
+void setup()
+{ 
+}
+
+void loop()
+{
+  static unsigned cnt = 0;
+  Serial.println(cnt++);
+  delay(50);
+}
---
+++ examples/Serial/Software/SerialReceiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using TeensySharp;
+
+namespace SerialReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watcher = new TeensyWatcher();
+            var teensy = watcher.ConnectedDevices.FirstOrDefault();  // Use the first teensy we find on the Bus                      
+            
+            SerialPort port = new SerialPort(teensy.Port,115200);           // open a serial port for the teensy            
+            port.Open();
+            
+            while(!Console.KeyAvailable)
+            {
+                Console.WriteLine(port.ReadLine());
+            }
+
+            port.Close();
+        }
+    }
+}
